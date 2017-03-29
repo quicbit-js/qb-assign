f1e730d80cf85a66bdc3e8639be7e62b78d0a189
mvoss9000 <mvoss9000@yahoo.com>
2017-03-29 19:54:35 -0400

    format and update bithound info

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-module.exports = function() {
-    var ret = Object(arguments[0])
-    for (var i = 1; i < arguments.length; i++) {
-        var src = arguments[i]
-        if (src != null) {
-            Object.keys(src).forEach(function (k) { ret[k] = src[k] })
-        }
+module.exports = function () {
+  var ret = Object(arguments[0])
+  for (var i = 1; i < arguments.length; i++) {
+    var src = arguments[i]
+    if (src != null) {
+      Object.keys(src).forEach(function (k) { ret[k] = src[k] })
     }
-    return ret
+  }
+  return ret
 }
-
